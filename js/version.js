13b967e6be75afe02299949704111ef7bfd49228
sastelvio <41056057+sastelvio@users.noreply.github.com>
2021-01-23 17:46:34 +0200

    Update version.js

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#data_version").text('2021.01.22');
-    $("#app_version").text('2.2');
+    $("#app_version").text('2.3');
 
     // CALL MODAL TO SHOW VERSION DATA   
 });
