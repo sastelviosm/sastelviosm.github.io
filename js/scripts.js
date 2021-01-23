5ecd6c4dfe2a31ec0ab7076b81b9b978a88fbb9f
sastelvio <41056057+sastelvio@users.noreply.github.com>
2021-01-23 12:34:30 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -48,9 +48,49 @@ $(function () {
      */
 
     $('header, .profile').on('click', '.menu-btn', function () {
-        $('.s_overlay').fadeIn();
-        $('.content-sidebar').addClass('active');
-        $('body,html').addClass('sidebar-open');
+
+        /* vars */
+        var width = $(window).width();
+        var id = $(this).attr('href');
+        var h = parseFloat($(id).offset().top);
+        var card_item = $(id);
+        var menu_items = $('.top-menu li');
+        var menu_item = $(this).closest('li');
+        var d_lnk = $('.lnks .lnk.discover');
+
+        if ((width >= 1024)) {
+
+            /* if desktop */
+            if (!menu_item.hasClass('active') & (width > 1023) & $('#home-card').length) {
+
+                /* close card items */
+                menu_items.removeClass('active');
+                container.find(card_items).removeClass('animated ' + animation_in);
+
+                if ($(container).hasClass('opened')) {
+                    container.find(card_items).addClass('animated ' + animation_out);
+                }
+
+                /* open card item */
+                menu_item.addClass('active');
+                container.addClass('opened');
+                container.find(card_item).removeClass('animated ' + animation_out);
+                container.find(card_item).addClass('animated ' + animation_in);
+
+                $(card_items).addClass('hidden');
+
+                $(card_item).removeClass('hidden');
+                $(card_item).addClass('active');
+            }
+        }
+        /* if mobile */
+        if ((width < 1024) & $('#home-card').length) {
+
+            /* scroll to section */
+            $('body,html').animate({
+                scrollTop: h - 76
+            }, 800);
+        }
         return false;
     });
     $('.content-sidebar, .container').on('click', '.close, .s_overlay', function () {
