4cde51d30467d752d515cb4802719eaae82d7ada
sastelvio <41056057+sastelvio@users.noreply.github.com>
2021-01-23 18:18:07 +0200

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
     $("#data_version").text('2021.01.22');
-    $("#app_version").text('2.3');
+    $("#app_version").text('2.2');
+
+    // CALL MODAL TO SHOW VERSION DATA   
 });
