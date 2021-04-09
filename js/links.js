f4d216b4f8f5bb3d5e50ecf950b76c003be8f127
sastelvio <41056057+sastelvio@users.noreply.github.com>
2021-04-09 19:37:49 +0200

    Add files via upload

--- js/links.js
+++ js/links.js
@@ -1,15 +1,39 @@
+//BLOG
 $('#ir_blog').click(function () {
     window.open('http://blog.sastelvio.com', '_blank');
 });
 
-$('#cv_pt').click(function () {
-    window.open('/files//CV_SASTELVIO_SERAFIM_MANUEL_Versao27.01.2021_Europass_PT.pdf', '_blank');
+//CV
+$("#cv_pt").on('click', function(e) {
+    e.preventDefault();
+    url = '/files/cv//CV_SASTELVIO_SERAFIM_MANUEL_Versao27.01.2021_Europass_PT.pdf';
+    window.open(url , "_blank");
+    trackOutboundLink(url);
 });
 
-$('#cv_en').click(function () {
-    window.open('/files//CV_SASTELVIO_SERAFIM_MANUEL_Versao27.01.2021_Europass_EN.pdf', '_blank');
+$("#cv_en").on('click', function(e) {
+    e.preventDefault();
+    url = '/files/cv//CV_SASTELVIO_SERAFIM_MANUEL_Versao27.01.2021_Europass_EN.pdf';
+    window.open(url , "_blank");
+    trackOutboundLink(url);
 });
 
+//CERTIFICATES AND LICENCES
+$("#certificate_Aeqso6kwZAAxuRwvHScgPLBEk5rz_en").on('click', function(e) {
+    e.preventDefault();
+    url = '/files/certificate/online//Certificado_Desenvolvimento_Agil_de_Software_Aeqso6kwZAAxuRwvHScgPLBEk5rz_EN.pdf';
+    window.open(url , "_blank");
+    trackOutboundLink(url);
+});
+
+$("#certificate_Aeqso6kwZAAxuRwvHScgPLBEk5rz_pt").on('click', function(e) {
+    e.preventDefault();
+    url = '/files/certificate/online//Certificado_Desenvolvimento_Agil_de_Software_Aeqso6kwZAAxuRwvHScgPLBEk5rz_PT.pdf';
+    window.open(url , "_blank");
+    trackOutboundLink(url);
+});
+
+//SOCIAL
 $('#social_twitter').click(function () {
     window.open('https://www.twitter.com/sstlv_seraph', '_blank');
 });
--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $("#data_version").text('2021.02.21');
-    $("#app_version").text('2.5');
+    $("#data_version").text('2021.04.09');
+    $("#app_version").text('2.6');
 
     // CALL MODAL TO SHOW VERSION DATA   
 });
