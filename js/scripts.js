d7a07a460ce71ea5f70d4309b679ede2e06511fa
sastelvio <41056057+sastelvio@users.noreply.github.com>
2021-04-10 09:02:51 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
  POPUP DE PDF
  */
 $(document).ready(function() {
- $(".iframe-popup").attr("href", "/files/certificate/online/Certificate_Desenvolvimento_Agil_de_Software_Aeqso6kwZAAxuRwvHScgPLBEk5rz_EN.pdf")
+ $(".iframe-popup").attr("href", "/files/certificate/online/Certificate_Desenvolvimento_Agil_de_Software_Aeqso6kwZAAxuRwvHScgPLBEk5rz_EN.")
     $('.iframe-popup').magnificPopup({
         type: 'iframe'
     });
